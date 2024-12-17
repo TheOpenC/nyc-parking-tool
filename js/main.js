851b787c0a2e88d01b5381ebc9ddc8aa2605b853
Drew Dudak <ddudak@gmail.com>
2024-12-16 21:20:40 -0500

    parking app

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // Public CORS proxy
+  const apiUrl = 'https://api.nyc.gov/public/api/GetCalendar?fromdate=12%2F12%2F2024&todate=01%2F01%2F2025';
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+  fetch(proxyUrl + apiUrl, {
+    method: 'GET',
+    headers: {
+      'Cache-Control': 'no-cache',
+      'Ocp-Apim-Subscription-Key': 'f900a38d921947fa920d239f7931049f'
+    }
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error('Network response was not ok');
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((err) => console.error('Error:', err));
 }
 
