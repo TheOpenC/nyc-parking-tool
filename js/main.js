d44dee5e02de506a97333202d4c1f87fb6b75652
Drew Dudak <ddudak@gmail.com>
2024-12-20 15:17:14 -0500

    Starting to work on formatting and additional features. Text Not integrated yet.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 // document.querySelector('button').addEventListener('click', getFetch);
 
-
+// Date info for parking API
 let today = new Date();
+let dayOfWeek = today.getDay()
 
 // this resolves formatting issues with dates
 let month = String(today.getMonth()+1).padStart(2, '0');
@@ -9,11 +10,12 @@ let day = String(today.getDate()).padStart(2, '0');
 let year = String(today.getFullYear());
 
 
+const parkingURL = `https://api.nyc.gov/public/api/GetCalendar?fromdate=${month}%2F${day}%2F${year}&todate=12%2F31%2F2025`;
 
-const apiUrl = `https://api.nyc.gov/public/api/GetCalendar?fromdate=${month}%2F${day}%2F${year}&todate=12%2F31%2F2025`;
+
  
-
-  fetch(apiUrl, {
+// Parking data
+  fetch(parkingURL, {
     method: 'GET',
     headers: {
       'Cache-Control': 'no-cache',
@@ -37,4 +39,55 @@ const apiUrl = `https://api.nyc.gov/public/api/GetCalendar?fromdate=${month}%2F$
       console.log(transformedData);
     })
     .catch((err) => console.error('Error:', err));
+
+
+const weatherURL = `https://api.weather.gov/points/40.6863,-73.9641`;
+
+    // Weather data
+    fetch(weatherURL)
+      .then(response => {
+        // check if the response is valid
+        if (!response.ok) throw new Error(`HTTP Error! status: ${response.status}`);
+        return response.json();
+      })
+      .then(data => {
+        console.log(data.properties)
+        // check if 'properties' exist and log the Forecast URL
+        if (data.properties && data.properties.forecast) {
+          const forecastURL = data.properties.forecast;
+          console.log(`Forecast URL:`, forecastURL)
+
+          // Fetch the forecast data
+          return fetch(forecastURL)
+            .then(response => {
+              
+              // check if the response is valid
+              if (!response.ok) throw new Error(`HTTP Error! status: ${response.status}`);
+              return response.json();
+            });
+          } else {
+            throw new Error(`Forecast URL not found in properties.`)
+          }
+        })
+        .then(forecastData => {
+              // Handle Forecast Data
+              const timedForecast = forecastData.properties.periods.map(period => {
+                return {
+                  number: period.number,
+                  name: period.name,
+                  isDaytime: period.isDaytime,
+                  temp: `${period.temperature}F`,
+                  forecast: period.detailedForecast
+                }
+              })
+              JSON.stringify(timedForecast, null, 2);
+              console.log(timedForecast);
+        })
+       // errors 
+        .catch(err => {
+          console.error(`Error fetching weather data:`, err.message);
+        });
+
+
+
   
