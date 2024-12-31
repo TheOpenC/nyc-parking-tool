4f7ad6e17bb3812634b4558c1ec2ea49a0925353
Drew Dudak <ddudak@gmail.com>
2024-12-30 20:56:50 -0500

    Date formatting consistent across forecast and parking info

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@
 let today = new Date();
 let dayOfWeek = today.getDay()
 
-// this resolves formatting issues with datess
+// this resolves formatting issues with dates
 let month = String(today.getMonth()+1).padStart(2, '0');
 let day = String(today.getDate()).padStart(2, '0');
 let year = String(today.getFullYear());
+
+
 
 
 const parkingURL = `https://api.nyc.gov/public/api/GetCalendar?fromdate=${month}%2F${day}%2F${year}&todate=12%2F31%2F2025`;
@@ -26,10 +28,21 @@ const parkingURL = `https://api.nyc.gov/public/api/GetCalendar?fromdate=${month}
     })
     .then((data) => {
       // the slice here gives us 8 days of parking information
+      // it would be good to come up with a way to change the date number string into just a day of the week + month/day/year
+
+
       let returnedData = data["days"].slice(0,8)
       const transformedData = returnedData.map(day => {
+        //transform the date data here. 
+        let pDay = (day.today_id.slice(6, 8)); //parseInt ensures it's an integer
+        let pMonth = (day.today_id.slice(4, 6)); // -1 converts the number to zero-based
+        let pYear = (day.today_id.slice(0, 4));
+        // create a new Date object
+        let pFormatted = new Date(pYear, pMonth, pDay)
+
         return {
-          today_id: day.today_id,
+          dateFormat: day.today_id,  //20241230 YearMonthDay
+          day: pFormatted.toLocaleDateString('en-US', {weekday: 'long', month: 'short', day: 'numeric', year: 'numeric'}),
           items: day.items[0]
         };
       });
@@ -70,11 +83,18 @@ const weatherURL = `https://api.weather.gov/points/40.6863,-73.9641`;
         .then(forecastData => {
               // Handle Forecast Data
               const timedForecast = forecastData.properties.periods.map(period => {
+                
+                //date format follows:  startTime: '2025-01-06T06:00:00-05:00'
+                let wDay = period.startTime.slice(8,10)
+                let wMonth = period.startTime.slice(5,7)
+                let wYear = period.startTime.slice(0,4)
+                let wFormatted = wYear+wMonth+wDay // this formats the forecast date the same way as the parking date 
                 return {
+                  test: wFormatted,
+                  rawData: period,
                   number: period.number,
-                  name: period.name,
-                  isDaytime: period.isDaytime,
-                  temp: `${period.temperature}F`,
+                  day: period.name,
+                  temp: `${period.temperature}°F`,
                   forecast: period.detailedForecast
                 }
               })
@@ -86,6 +106,9 @@ const weatherURL = `https://api.weather.gov/points/40.6863,-73.9641`;
           console.error(`Error fetching weather data:`, err.message);
         });
 
+class Day {
+  yearMonthDay: 
 
+}
 
   
